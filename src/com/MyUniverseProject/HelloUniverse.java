031ac2ff8191da24086ad4c4e265f0a7a8b82e30
Sadjo Peguy Nya <peguynya@gmail.com>
2023-01-04 18:27:03 +0100

    Initial version of my project "variables declarations + java class added"

---
+++ src/com/MyUniverseProject/HelloUniverse.java
@@ -0,0 +1,12 @@
+package com.MyUniverseProject;
+
+public class HelloUniverse {
+    public static void main(String... args){
+        int nombreDePlanetes = 8;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:"+nombreDePlanetes);
+        nombreDePlanetes = 9;
+        System.out.println("  Il y a quelques années cependant, elles étaient au nombre de:"+nombreDePlanetes);
+
+    }
+
+}
