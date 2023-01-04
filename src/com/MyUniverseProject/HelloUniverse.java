c594234382df8fc1984ca9b1d575f23ce77aa7ce
Sadjo Peguy Nya <peguynya@gmail.com>
2023-01-04 19:00:07 +0100

    characters declaration and initialization

--- src/com/MyUniverseProject/HelloUniverse.java
+++ src/com/MyUniverseProject/HelloUniverse.java
@@ -2,10 +2,15 @@ package com.MyUniverseProject;
 
 public class HelloUniverse {
     public static void main(String... args){
-        int nombreDePlanetes = 8;
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:"+nombreDePlanetes);
-        nombreDePlanetes = 9;
-        System.out.println("  Il y a quelques années cependant, elles étaient au nombre de:"+nombreDePlanetes);
+        String valeur = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        int nbPlanetes=9;
+        System.out.println(valeur);
+        System.out.println(nbPlanetes);
+        valeur = "Il y a quelques années cependant, elles étaient au nombre de : ";
+        nbPlanetes=8;
+        System.out.println(valeur);
+        System.out.println(nbPlanetes);
+
 
     }
 
