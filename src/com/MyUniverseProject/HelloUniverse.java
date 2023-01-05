ec5ac24351fd10388b18d6dffb916389ad9a3181
Sadjo Peguy Nya <peguynya@gmail.com>
2023-01-05 15:48:35 +0100

    for and switch case conditions

--- src/com/MyUniverseProject/HelloUniverse.java
+++ src/com/MyUniverseProject/HelloUniverse.java
@@ -1,17 +1,27 @@
 package com.MyUniverseProject;
 
+import javax.swing.*;
+
 public class HelloUniverse {
     public static void main(String... args){
-        String phraseIntroduction="En %d, les planètes du système solaire étaient au nombre de : %d";
-        int nbPlanetes=0;
-        short annee=2006;
-        if (annee<2006){
-            nbPlanetes=9;
-        }
-        else{
-            nbPlanetes=8;
-        }
-        System.out.printf(phraseIntroduction,annee,nbPlanetes);
+
+             int nbPlanetes;
+             for(nbPlanetes=7;nbPlanetes<=9;nbPlanetes++){
+                 switch(nbPlanetes){
+                     case 7:
+                         System.out.printf("On sait qu'au 16ème siècle, seules %d planètes avaient été découvertes",nbPlanetes);
+                         break;
+                     case 8 :
+                         System.out.printf("On sait que le nombre de planètes est passé de %d à %d au 17ème siècle, mais il a également été réduit de %d à 8 en 2006",7,nbPlanetes,9);
+                         break;
+                     case 9 :
+                         System.out.printf("On sait que le nombre de planètes est passé de 8 à %d au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8",nbPlanetes);
+                         break;
+                     default :
+                         System.out.printf("Le programme ne peut pas fournir de résultat pour %d",nbPlanetes);
+                 }
+             }
+
 
 
 
